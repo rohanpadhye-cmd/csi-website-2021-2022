88096205999c979370c449724835bb49e99e2596
Mcvean Soans <mc.soans02@gmail.com>
2021-02-20 23:02:01 +0530

    Contact Form Created

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,109 @@
+import { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+const ContactForm = () => {
+    
+    const [name, setName] = useState(''); // Name
+    const [role, setRole] = useState('Visitor'); // Role
+    const [studentClass, setStudentClass] = useState(''); // Student Class
+    const [facultyDpt, setfacultyDpt] = useState(''); // Faculty Department
+    const [email, setEmail] = useState(''); // Email ID
+    const [body, setBody] = useState(''); // Text Body
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+    
+    const handleSubmit = (e) => {
+
+        const submission = { name, role, studentClass, facultyDpt, email, body };
+        
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+        console.log("Submitted");
+        e.preventDefault();
+    }
+
+    return (
+        <Form onSubmit={handleSubmit} name="contact" method="POST">
+            <input type="hidden" name="form-name" value="contact" />
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="role" className="mt-4">
+                <Form.Label>Role</Form.Label>
+                <Form.Control as="select" value={role} onChange={(e) => setRole(e.target.value)}>
+                    <option>Visitor</option>
+                    <option>Student</option>
+                    <option>Faculty</option>
+                </Form.Control>
+            </Form.Group>
+
+            {
+                role === 'Student' ? (
+                    <Form.Group controlId="studentClass" className="mt-4">
+                        <Form.Label>Class</Form.Label>
+                        <Form.Control type="text" placeholder="Your Class" value={studentClass} onChange={(e) => setStudentClass(e.target.value)} />
+                    </Form.Group>
+                ) : (<div />)
+            }
+
+            {
+                role === 'Faculty' ? (
+                    <Form.Group controlId="studentClass" className="mt-4">
+                        <Form.Label>Department</Form.Label>
+                        <Form.Control as="select" value={facultyDpt} onChange={(e) => setfacultyDpt(e.target.value)}>
+                            <option>IT</option>
+                            <option>CMPN</option>
+                            <option>EXTC</option>
+                            <option>ETRX</option>
+                            <option>INST</option>
+                            <option>Humanities &amp; Applied Sciences</option>
+                            <option>AI &amp; Data Science</option>
+                            <option>MCA | ME</option>
+                        </Form.Control>
+                    </Form.Group>
+                ) : (<div />)
+            }
+
+            <Form.Group controlId="email" className="mt-4">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="body" className="mt-4">
+                <Form.Label>How can we help you?</Form.Label>
+                <Form.Control as="textarea" rows={4} type="email" placeholder="Your Text Here" value={body} onChange={(e) => setBody(e.target.value)} />
+            </Form.Group>
+            
+            <Button className="contact-btn mt-2" variant="success" type="submit">
+                Submit
+            </Button>
+
+            <h1>{name}</h1>
+        </Form>
+    );
+}
+
 const ContactComponent = () => {
     return (
-        <h1>
-            Contact Page!
-        </h1>
+        <Container>
+            <Row>
+                <Col sm lg={{span: 5, offset: 7}}>            
+                    <ContactForm />
+                </Col>
+            </Row>
+        </Container>
     );
 }
  
--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,16 +1,18 @@
-import {IconContext} from 'react-icons'
-import { FaEnvelope, FaFacebook, FaInstagram, FaLinkedinIn,FaHeart} from 'react-icons/fa';
-const FooterComponent=()=>{
+import { IconContext } from 'react-icons';
+import { FaEnvelope, FaFacebook, FaInstagram, FaLinkedinIn, FaHeart} from 'react-icons/fa';
+
+const FooterComponent = () => {
+
     return(
         <footer className='d-flex'>
             <div className='ml-5 my-auto'>&copy;CSI-VESIT 2020</div>
             <div className='mx-auto my-auto'>Made with <FaHeart/> by CSI Web Team</div>
             <div className='mr-5'>
-                <IconContext.Provider value={{ color:'#363537',className: "fa-icons",size:'2em' }}>
-                    <a href='' className='ml-2'><FaEnvelope/></a>
-                    <a href='' className='ml-2'><FaInstagram/></a>
-                    <a href='' className='ml-2'><FaFacebook/></a>
-                    <a href='' className='ml-2'><FaLinkedinIn/></a>
+                <IconContext.Provider value={{ color: '#363537', className:'fa-icons', size:'2em' }}>
+                    <a href='https://www.google.com/' className='ml-2'><FaEnvelope/></a>
+                    <a href='https://www.google.com/' className='ml-2'><FaInstagram/></a>
+                    <a href='https://www.google.com/' className='ml-2'><FaFacebook/></a>
+                    <a href='https://www.google.com/' className='ml-2'><FaLinkedinIn/></a>
                 </IconContext.Provider>               
             </div>
         </footer>
