b3de6621940a5328856f6b24df1109165316f1f0
Akhil Chakkungal <Akhilchakkungal@gmail.com>
2021-02-28 23:23:08 +0530

    Contact Component

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-import {IconContext} from 'react-icons'
-import { FaEnvelope, FaFacebook, FaInstagram, FaLinkedinIn,FaHeart} from 'react-icons/fa';
-=======
 import { Container, Row, Col } from 'react-bootstrap';
 import { GiHeartBattery, GiCoffeeMug } from 'react-icons/gi';
->>>>>>> 51011402a18be90b908c0b863a6e72d60506e52d
 
 const FooterComponent=()=>{
 
     return(
-<<<<<<< HEAD
-        <footer className='d-flex'>
-            <div className='ml-5 my-auto'>&copy;CSI-VESIT 2020</div>
-            <div className='mx-auto my-auto'>Made with <FaHeart/> by CSI Web Team</div>
-            <div className='mr-5'>
-                <IconContext.Provider value={{ color:'#363537',className: "fa-icons",size:'2em' }}>
-                    <a href='http://mail.google.com/mail/?view=cm&fs=1&tf=1&to=csi.vesit@ves.ac.in' className='ml-2'><FaEnvelope/></a>
-                    <a href='https://www.instagram.com/csi_vesit/?hl=en' className='ml-2'><FaInstagram/></a>
-                    <a href='https://www.facebook.com/csivesit/' className='ml-2'><FaFacebook/></a>
-                    <a href='https://www.linkedin.com/company/csi-vesit/' className='ml-2'><FaLinkedinIn/></a>
-
-                </IconContext.Provider>               
-            </div>
-=======
         <footer>
             <Container fluid className="d-none d-md-block">
                 <Row>
@@ -204,7 +185,6 @@ const FooterComponent=()=>{
                     </div>
                 </Row>
             </Container>
->>>>>>> 51011402a18be90b908c0b863a6e72d60506e52d
         </footer>
     )
 }
