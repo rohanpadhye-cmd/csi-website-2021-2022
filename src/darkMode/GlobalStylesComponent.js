5a25c4019ad0e83c995447436c344f05ca82167a
Mcvean Soans <mc.soans02@gmail.com>
2021-03-01 10:16:06 +0530

    Contact Page: Minor fixes

--- src/darkMode/GlobalStylesComponent.js
+++ src/darkMode/GlobalStylesComponent.js
@@ -6,12 +6,12 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
     transition: all 0.5s linear;
   }
-
+  
   hr {
     background: ${({ theme }) => theme.text};
     transition: all 0.50s fade;
   }
-
+  
   .modal-header {
     color: ${({ theme }) => theme.body};
     background-color: ${({ theme }) => theme.text};
@@ -20,5 +20,10 @@ export const GlobalStyles = createGlobalStyle`
   .modal-content {
     background-color: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
+  }
+  
+  .carousel-control-next-icon, .carousel-control-prev-icon {
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.text};
   }
 `
