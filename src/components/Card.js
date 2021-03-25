8bc40241e19de1f23ad906e84a861f5a50c73bd2
Aniket31-coder <55101835+Aniket31-coder@users.noreply.github.com>
2021-03-25 15:04:16 +0530

    Added Initial Animations | Updated Jumbotron
    * Added Animations To All Pages
    
    * Delete package-lock.json

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 /*import React from "react";
 import {Card} from "react-bootstrap";
 import { IconContext } from 'react-icons';
-import { FaEnvelope, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
 
 
 function Cards(props) {
@@ -9,16 +9,16 @@ function Cards(props) {
 
   return (
     <div>
-    <Card style={{ width: '18rem' }} className="councilCard" >
+    <Card style={{ width: '18rem' } } >
       <Card.Img variant="top" src={props.imageUrl} style={{ height:"200px" }}/>
-      <Card.Body className="councilCardText">
+      <Card.Body className="cardText">
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>{props.role}</Card.Text>
         <IconContext.Provider value={{ color: '#363537', className:'fa-icons', size:'2em' }}>
-          <a href='https://www.google.com/' className='ml-2'><FaInstagram /></a>
-          <a href='mailto:csi.vesit@ves.ac.in' className='ml-2'><FaEnvelope /></a>
-          <a href='https://www.google.com/' className='ml-2'><FaLinkedinIn /></a>
-        </IconContext.Provider>
+                    <a href='https://www.google.com/' className='ml-2'><FaInstagram/></a>
+                    <a href='https://www.google.com/' className='ml-2'><FaGithub/></a>
+                    <a href='https://www.google.com/' className='ml-2'><FaLinkedinIn/></a>
+                </IconContext.Provider>
       </Card.Body>
     </Card>
 
