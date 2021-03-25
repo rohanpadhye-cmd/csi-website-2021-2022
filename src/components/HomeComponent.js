b0adfba7818afd3907bc12ce82627002615814d9
ShreyasP7 <72619439+ShreyasP7@users.noreply.github.com>
2021-03-25 12:48:18 +0530

    Home Page Modal Completed
    * Added Form inside Modal for workshop registration
    
    * Added margin to CSI Member Label
    
    * Membership Form
    
    Co-authored-by: Mihir <ranemihir3@gmail.com>
    Co-authored-by: Mcvean Soans <69250084+mcs-codes@users.noreply.github.com>

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,16 +1,51 @@
-import { Jumbotron, Container, Card, Button, Col, Row} from 'react-bootstrap';
+
+import { Jumbotron, Container, Card, Button, Col, Row, Modal, Form} from 'react-bootstrap';
+import { useState } from 'react';
 import VES from "../assets/back-img.jpeg";
 import { Parallax, Background } from "react-parallax";
 import groupImg from "../assets/group-img.jpg";
 import poster from "../assets/poster.jpeg";
+import QR from "../assets/QR_code.jpg";
 
 
     
 const HomeComponent = () => {
+
+
+  const [showMembershipModal, setModal] = useState(false);
+  const [membershipTaken,setMembership]=useState(false);  //Handle the case when user will press the submit button in Membership form
+  const handleSubmit = () => {
+    setMembership(true);
+  }
+  const handleClose = () => 
+  {
+    setModal(false);
+    setMembership(false);
+  }
+  const handleShow = () => setModal(true);
+
+
+
+  const [showWorkshopModal, setShowWorkshopModal] = useState(false);
+  const [isWorkshopSubmitted,setIsWorkshopSubmitted]=useState(false);
+  const [isCSIMember,setIsCSIMember]=useState('Yes');
+
+  const handleWorkshopSubmit = () => setIsWorkshopSubmitted(true);
+
+  const handleWorkshopClose = () => {
+    setShowWorkshopModal(false);
+    setIsWorkshopSubmitted(false);
+    setIsCSIMember('Yes');
+  };
+
+  const handleshowWorkshopModal = () => setShowWorkshopModal(true);
+
+  const CSIMemberDropdown = (e) =>  setIsCSIMember(e.target.value);
+
     return (
     <div className="home">
 
-        <Container fluid className="m-0">
+        <Container fluid className="m-0">        
           <Jumbotron>
           <Parallax bgImage= {VES} strength={200}>
             <div style={{height: 400}}>
@@ -59,11 +94,277 @@ const HomeComponent = () => {
           </Col>
           <Col lg={5}>
             <Card.Body>
-              <Button className="registration-buttons " variant="info" size="lg">Membership Form</Button>
-              <Button className="registration-buttons " variant="info" size="lg">Register for workshop/events </Button>
+              <Button className="registration-buttons " variant="info" size="lg" onClick={handleShow}>Membership Form</Button>
+              <Button className="registration-buttons " variant="info" size="lg" onClick={handleshowWorkshopModal}>Register for workshop/events </Button>
+       
+
+              <Modal
+                size="lg"
+                show={showWorkshopModal}
+                onHide={handleWorkshopClose}            
+              >
+                {!isWorkshopSubmitted ? 
+                  <>
+                  <Modal.Header closeButton>
+                    <Modal.Title>
+                      CSI-VESIT PHP & LARAVEL WORKSHOP <br />
+                     <p style={{fontSize:"15px"}}>CSI-VESIT presents PHP & LARAVEL workshop which will enable users to build robust and swift web applications having a speed improvement of about 700% as compared to other websites!<br />
+                      <br />
+                      Date : 20th and 21st February, 2021<br />
+                      Time : 11am to 1pm<br />
+                      Platform : Google Meet<br />
+                      <br />
+                      NOTE: This workshop is exclusively for CSI members.</p>
+                    </Modal.Title>
+                  </Modal.Header>
+
+                    <Modal.Body>
+                      <Form onSubmit={handleWorkshopSubmit}> 
+
+                        <Form.Row>
+                          <Form.Group as={Col} xs="6" controlId="FirstName">
+                            <Form.Label><h5>First Name</h5></Form.Label>
+                            <Form.Control
+                            type="text"
+                            placeholder="Your First Name"
+                            required />
+                          </Form.Group>
+
+                          <Form.Group as={Col} xs="6" controlId="LastName">
+                            <Form.Label><h5>Last Name</h5></Form.Label>
+                            <Form.Control
+                            type="text"
+                            placeholder="Your Last Name"
+                            required />
+                          </Form.Group>
+                        </Form.Row>
+
+                        <Form.Row>
+                          <Form.Group as={Col} xs="6" controlId="Class">
+                            <Form.Label><h5>Class</h5></Form.Label>
+                            <Form.Control
+                            type="text"
+                            placeholder="Your Class"
+                            required />
+                          </Form.Group>
+
+                          <Form.Group as={Col} xs="6" controlId="ContactNo">
+                            <Form.Label><h5>Contact No.</h5></Form.Label>
+                            <Form.Control
+                            type="text"
+                            placeholder="Your Contact No."
+                            required />
+                          </Form.Group>                          
+                        </Form.Row>
+
+                        <Form.Row >
+                          <Form.Group as={Col} as={Row} xs="6" controlId="CSIMember">
+                            <Form.Label className="ml-1" column lg="6" xs="6"><h5>CSI Member?</h5></Form.Label>
+                            <Col xs="6">
+                            <Form.Control as="select" defaultValue="Choose..." onChange={CSIMemberDropdown}>
+                              <option>Yes</option>
+                              <option>No</option>
+                            </Form.Control>
+                            </Col>
+                          </Form.Group>
+                        </Form.Row>
+
+                        <hr></hr>
+
+                        <Form.Row>
+                          <Form.Group controlId="CSIMember">
+                            <Button variant="primary" type="submit" >Submit</Button>
+                            <Button className="ml-3" variant="secondary" onClick={handleWorkshopClose}>Close</Button>
+                          </Form.Group>
+                        </Form.Row>
+
+                      </Form>
+                    </Modal.Body>
+                  </> : 
+                  <>
+                    <Modal.Header closeButton>
+                      CSI-VESIT PHP & LARAVEL WORKSHOP 
+                    </Modal.Header>
+
+                    <Modal.Body>
+                      {isCSIMember === 'Yes' ? <h5>You have successfully registered for the workshop.</h5> : <h5>Please fill the Membership form and then you can register for this workshop </h5>}
+                      <hr></hr>
+                      <Button variant="secondary" onClick={handleWorkshopClose}>Close</Button>
+                    </Modal.Body> 
+                  </>
+                }
+              </Modal>
             </Card.Body>
           </Col>
         </Row>
+
+        {/* Modal for Membership Form */}
+        <Modal className="registration-modal" show={showMembershipModal} onHide={handleClose} animation={true} size="lg">
+      { !membershipTaken? 
+       <>
+        <Modal.Header closeButton>
+                <Modal.Title>
+                          CSI-VESIT MEMBERSHIP FORM<br/>
+                          <p style={{fontSize:"15px"}}>For any Queries ,CONTACT:<br/>
+                          Fayzaan Qureshi ( Co-Chairperson) - 9819186523<br/>
+                          Saloni Ingle (Jr. Treasurer) - 9370550921<br/>
+                          Saurav Telge(Operations Secretary) - 9821505562
+                          <br/><br/>
+                          NOTE : There will not be a Resubmit options so Please fill the Form correctly
+                          <br/><br/>
+                          The name and photo associated with your Google account will be recorded when you upload files and submit this form.</p>
+                </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+                    
+            <Form onSubmit={handleSubmit}>
+                  <Form.Row>
+                     <Form.Group as={Col} xs="6">
+                         <Form.Label><h5>Email Address</h5></Form.Label>
+                         <Form.Control 
+                         type="email" 
+                         placeholder="Enter Email Address" 
+                         required/>
+                      </Form.Group>
+                  </Form.Row>
+
+                      <Form.Row>
+                          <Form.Group as={Col} xs="6" controlId="FirstName">
+                            <Form.Label><h5>First Name</h5></Form.Label>
+                            <Form.Control
+                            type="text"
+                            placeholder="Your First Name"
+                            required />
+                          </Form.Group>
+
+                          <Form.Group as={Col} xs="6" controlId="LastName">
+                            <Form.Label><h5>Last Name</h5></Form.Label>
+                            <Form.Control
+                            type="text"
+                            placeholder="Your Last Name"
+                            required />
+                          </Form.Group>
+                      </Form.Row>
+
+
+                      <Form.Row>
+                        <Form.Group as={Col} md="4" xs="6">
+                              <Form.Label className="form-label"><h5>Date Of Birth</h5></Form.Label>
+                              <Form.Control type="date" name="dob" placeholder="Date Of Birth" required></Form.Control>
+                        </Form.Group>
+                       <Form.Group as={Col} md="4" xs="6">
+                               <Form.Label className="form-label"><h5>Class</h5></Form.Label>
+                               <Form.Control as="select" defaultValue="" required>
+                                 <option>D6</option>
+                                 <option>D7A</option>
+                                 <option>D7B</option>
+                                 <option>D7C</option>
+                                 <option>D7A</option>
+                                 <option>D8</option>
+                                 <option>D9A</option>
+                                 <option>D9B</option>
+                                 <option>D10A</option>
+                                 <option>D10B</option>
+                                 <option>MCA</option>
+                               </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} md="4" xs="6">
+                          <Form.Label className="form-label"><h5>Gender</h5></Form.Label>
+                          <Form.Control as="select" defaultValue="" required>
+                            <option>Male</option>
+                            <option>Female</option>
+                            <option>Others</option>
+                          </Form.Control>
+                        </Form.Group>
+
+                     
+                        <Form.Group as={Col} md="4" xs="6">  
+                           <Form.Label className="form-label"><h5>Contact No</h5></Form.Label>
+                            <Form.Control 
+                            type="text" 
+                            placeholder="Your Contact No." 
+                            required/>
+                        </Form.Group>
+
+                        <Form.Group as={Col} md="4" xs="6">
+                           <Form.Label className="form-label"><h5>Degree</h5></Form.Label>
+                           <Form.Control as="select" 
+                           defaultValue="" 
+                           required>
+                             <option>BE</option>
+                             <option>MCA</option>
+                           </Form.Control>
+                        </Form.Group>
+
+                       <Form.Group as={Col} md="4" xs="6">
+                         <Form.Label className="form-label"><h5>Year</h5></Form.Label>
+                         <Form.Control 
+                         as="select" 
+                         defaultValue="" 
+                         required>
+                           <option>SE</option>
+                           <option>TE</option>
+                           <option>FE</option>
+                         </Form.Control>
+                       </Form.Group>
+                         
+                    </Form.Row>
+                      
+                    <Form.Label className="form-label"><h5>Payment Method(To above mobile number only i.e 9370550921)</h5></Form.Label>   
+                    <Form.Row>  
+                      <Form.Group as={Col} xs="4">
+                         
+                         <Form.Control as="select" defaultValue="">
+                         <option>Google Pay</option>
+                         <option>Paytm</option>
+                         <option>PhonePe</option>
+                         <option>Other</option>
+                         </Form.Control>
+                         <Form.Text className="text-muted">
+                         Write for csi membership_Name_class in what's this for of gpay
+                         </Form.Text>
+                         
+                      </Form.Group>
+                    </Form.Row>
+                    
+
+                      <Form.Group>
+                         <Form.Label className="form-label"><h5>Use this QR code for PhonePe ,Patym or any other UPI Payment options</h5></Form.Label>
+                         <Row>
+                            <Col>
+                            <img className="mx-auto" src={QR} style={{display:"block"}}/>
+                            </Col>
+                         </Row>
+                      </Form.Group>  
+
+                      <Form.Group>
+                         <Form.File className="position-relative" required name="file" label="Screenshots/Receipt of Payment completed " feedbackTooltip/>
+                      </Form.Group>
+                      <hr/>
+                      <Form.Row>
+                          <Form.Group>
+                            <Button variant="primary" type="submit">Submit</Button>
+                            <Button className="ml-3" variant="secondary" onClick={handleClose}>Close</Button>
+                          </Form.Group>
+                      </Form.Row>
+
+                </Form>
+        </Modal.Body>
+                  
+                </>  :
+                <>
+                <Modal.Header closeButton>
+                      CSI-VESIT MEMBERSHIP FORM
+                </Modal.Header>
+                <Modal.Body>
+                      <h5>Thanks For Submitting Form</h5>
+                      <hr/>
+                      <Button variant="secondary" onClick={handleClose}>Close</Button>
+                </Modal.Body>
+                       
+                    
+                </> }
+          </Modal> 
         
         
     </div>
