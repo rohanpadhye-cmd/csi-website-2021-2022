7ee46d50fed4049577cc960691ba66180990dee9
MeetPatelLearning <78213673+MeetPatelLearning@users.noreply.github.com>
2021-08-11 10:16:49 +0530

    Council Page (#45)
    * responsive, no greyscale, redesigned cards without carousel
    
    * card's design2 rejected
    
    * card's design3 along with carousel+responsive
    
    * resolve conflicts part2 final
    
    Co-authored-by: Your Name <Your@name>

--- src/components/CouncilComponent.js
+++ src/components/CouncilComponent.js
@@ -1,93 +1,387 @@
-import React,{useState,useRef} from "react";
-import { Container, Row, Col,Carousel ,Button} from "react-bootstrap";
-import Faculty from "./council/Faculty";
-import FinalYearCouncil from "./council/FinalYearCouncil";
-import ThirdYearCouncil from "./council/ThirdYearCouncil";
-import SecondYearCouncil from "./council/SecondYearCouncil";
+import React from "react";
+import CouncilCard from "../components/council/CouncilCard"
+import FacultyCard from "../components/council/FacultyCard"
+import { Container, Row, Carousel, Button } from "react-bootstrap";
+import "../components/council/councilCard.css"
 
+const facultyDetails = [
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "Pratik Vartak",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "Sarah Jhonson",
+    },
+  ];
+ 
+const top3MemberDetails = [
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    }, 
+  ]; 
+const beMemberDetails = [
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "POM",
+    },
+  ];
+  
+const top5MemberDetails = [
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "top5",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "top5",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "top5",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "top5",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "top5",
+    },
+  ];
+const teMemberDetails = [
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: " Officer",
+      name: "XOM",
+    },
+  ];
 
+const seMemberDetails = [
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+    {
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMKi6B7dQxVyrN_qgvVWCX96omLGEtmUXK0A&usqp=CAU",
+      role: "Sr.Web Officer",
+      name: "KOM",
+    },
+  ];
+
+  let facultyCards = facultyDetails.map((info, index) => {
+      return (
+          <FacultyCard 
+          imageUrl={info.imageUrl}
+              role={info.role}
+              name={info.name}
+              key={index}
+              />
+              )
+  });
+
+  let top3MemberCards = top3MemberDetails.map((info, index) => {
+    return (
+        <CouncilCard
+            imageUrl={info.imageUrl}
+            name={info.name}
+            role={info.role}
+            key={index}
+        />
+    )
+  });
+  let beMemberCards = beMemberDetails.map((info, index) => {
+      return (
+          <CouncilCard
+              imageUrl={info.imageUrl}
+              name={info.name}
+              role={info.role}
+              key={index}
+          />
+      )
+  });
+
+  let top5MemberCards = top5MemberDetails.map((info, index) => {
+      return (
+          <CouncilCard
+              imageUrl={info.imageUrl}
+              name={info.name}
+              role={info.role}
+              key={index}
+          />
+      )
+  });
+  let teMemberCards = teMemberDetails.map((info, index) => {
+      return (
+          <CouncilCard
+              imageUrl={info.imageUrl}
+              name={info.name}
+              role={info.role}
+              key={index}
+          />
+      )
+  });
+
+  let seMemberCards = seMemberDetails.map((info, index) => {
+      return (
+          <CouncilCard
+              imageUrl={info.imageUrl}
+              name={info.name}
+              role={info.role}
+              key={index}
+          />
+      )
+  });
+
+  const Faculty = () => {
+      return (
+        <p>
+          <h1 className="mb-5" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="500">Faculty Members</h1>
+          <div class="map-team-cards faculty-card">
+          {facultyCards}
+          </div>
+        </p>
+      );
+  }
+
+  const BEYearCouncil = () => {
+    return (
+      <>
+        <div>  
+            <h1 className="mb-4" style={{alignText:"center"}}>TOP 3</h1>
+            <div className="map-team-cards">
+            {top3MemberCards}
+            </div>
+        </div>
+        <div>  
+            <h1 className="mb-4" style={{alignText:"center"}}>BE Council Members</h1>
+            <div className="map-team-cards">
+            {beMemberCards}
+            </div>
+        </div>
+      </>
+      );
+  }
+
+  const TEYearCouncil = () => {
+    return (
+        <>
+        <div>  
+            <h1 className="mb-4" style={{alignText:"center"}}>TOP 5</h1>
+            <div className="map-team-cards">
+            {top5MemberCards}
+            </div>
+        </div>
+        <div>  
+            <h1 className="mb-4" style={{alignText:"center"}}>TE Council Members</h1>
+            <div className="map-team-cards">
+            {teMemberCards}
+            </div>
+        </div>
+        </>
+      );
+  }
+
+  const SEYearCouncil = () => {
+    return (
+        <div>  
+            <h1 className="mb-4" style={{alignText:"center"}}>SE Council Members</h1>
+            <div className="map-team-cards">
+            {seMemberCards}
+            </div>
+        </div>
+      );
+  }
 
 const CouncilComponent = () => {
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = React.useState(0);
+  const handleSelect = (selectedIndex, e) => {setIndex(selectedIndex); };
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
-
-
-
-  
   return (
-    <Container >
-
-      <h1 className="mb-4">Faculty Members</h1>
-      <Row>
-        <Col />
+    <Container>
+      <Carousel
+        style={{ marginTop:"50px" }}
+        data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1000"
+        indicators={false}
+        controls={false}
+      >
         <Faculty />
-        <Col />
+      </Carousel>
+
+      <Row className="justify-content-center" style={{marginBottom:"50px"}} data-aos="fade-up" data-aos-easing="linear" data-aos-duration="500">
+        <Button
+          className={`ml-2 w-25  ${index === 0 ? "contact-btn " : null} `}
+          onClick={() => setIndex(0)}
+        >BE Council
+        </Button>
+        <Button
+          className={`ml-2 w-25  ${index === 1 ? "contact-btn " : null} `}
+          onClick={() => setIndex(1)}
+        >TE Council
+        </Button>
+        <Button
+          className={`ml-2 w-25  ${index === 2 ? "contact-btn " : null} `}
+          onClick={() => setIndex(2)}
+        >SE Council
+        </Button>
       </Row>
-      
 
-    <Row className='justify-content-center'>
-    <Button 
-    className={`ml-2 w-25  ${index===0?  "contact-btn " : null} `} 
-    onClick={() => setIndex(0)}>
-        BE
-    </Button>
-    <Button 
-    className={`ml-2 w-25  ${index===1?  "contact-btn " : null} `} 
-    onClick={() => setIndex(1)}>
-      TE
-    </Button>
-    <Button 
-    className={`ml-2 w-25  ${index===2?  "contact-btn " : null} `}
-    onClick={() => setIndex(2)}>
-      SE
-    </Button>
-    </Row>
-    <Carousel activeIndex={index} onSelect={handleSelect} interval={2000} indicators={false} controls={false}>
-    <Carousel.Item>
-        <h1 className="mt-3 mb-4">BE Council Members</h1>
-          <Row>
-          <FinalYearCouncil/>
-          </Row>
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        interval={2000}
+        indicators={false}
+        controls={false}
+      >
+        <Carousel.Item>
+          <BEYearCouncil />
         </Carousel.Item>
         <Carousel.Item>
-        <h1 className="mt-3 mb-4">TE Council Members</h1>
-          <Row>
-          <ThirdYearCouncil/>
-          </Row>
+          <TEYearCouncil />
         </Carousel.Item>
         <Carousel.Item>
-        <h1 className="mt-3 mb-4">SE Council Members</h1>
-          <Row>
-          <SecondYearCouncil/>
-          </Row>
+          <SEYearCouncil />
         </Carousel.Item>
-    </Carousel>
-    
-
-
-
-    
-      {/* <OwlCarousel {...options}>
-        
-      </OwlCarousel>
-
-      
-      <OwlCarousel {...options}>
-      
-      </OwlCarousel>
-
-      
-      <OwlCarousel {...options}>
-       
-      </OwlCarousel> */}
-
-      
+      </Carousel>
     </Container>
-  )
-}
+  );
+};
+
  
 export default CouncilComponent;
--- src/components/council/CouncilCard.js
+++ src/components/council/CouncilCard.js
@@ -1,23 +1,35 @@
 import "./councilCard.css";
 import { Col } from "react-bootstrap";
 import { FaGithub, FaInstagram, FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
-import P1 from "../../assets/P1.jpeg"
-
-const CouncilCard = ({ name,imageUrl }) => {
+import temp from "./Saurav_Telge-rm-bg.png";
+const CouncilCard = (props) => {
   return (
-    <Col md={4} xs={6} className="mb-5">
-      <div class="img-wrapper">
-        <img src={imageUrl} alt="Atul Prajapati" />
-        <h2>{name}</h2>
-        <ul>
-          <li><a href="https://www.instagram.com/atulkprajapati2000/"><FaInstagram /></a></li>
-          <li><a href="https://github.com/atuljustano"><FaGithub /></a></li>
-          <li><a href="https://twitter.com/atuljustano"><FaLinkedinIn /></a></li>
-          <li><a href="https://twitter.com/atuljustano"><FaEnvelope /></a></li>
-        </ul>
-      </div>
+    <Col md={4} sm={6} xs={12} className="mb-5">
+      {/* <div class="card-wrapper" style={{ backgroundColor: `${props.bgcol}` }}> */}
+        <div class="card">
+          <div class="card-image">
+            <img src={temp} alt="Council-img" />
+            {/* <img src={props.imageUrl} alt="Council-img" /> */}
+            <div class="social-icons">
+              <ul>
+                <li><a href="https://www.instagram.com/atulkprajapati2000/"><FaInstagram /></a>
+                </li>
+                <li><a href="https://github.com/atuljustano"><FaGithub /></a>
+                </li>
+                <li><a href="https://twitter.com/atuljustano"><FaLinkedinIn /></a>
+                </li>
+                <li><a href="https://twitter.com/atuljustano"><FaEnvelope /></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="details">
+              <h4>{props.name}</h4>
+              <span class="job-title">{props.role}</span>
+          </div>
+        </div>
     </Col>
-  )
+  );
 }
 
 export default CouncilCard;
